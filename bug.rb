a7e11189887616d9ae9b1d7709bf18afeb49dea1
BugHunter-X <askzenai@gmail.com>
2024-12-08 17:14:09 +0100

    Add bug.rb file

---
+++ bug.rb
@@ -0,0 +1,16 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # Output: 10
+my_object.value = 20  # This will NOT work as value is a read-only method
+puts my_object.value # Output: 10
+```
